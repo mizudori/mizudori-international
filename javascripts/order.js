6cd78b5dc5936b35741ab9487466fc98d827dd05
Henry Addo <addhen@linuxmail.org>
2014-03-31 20:53:44 +0900

    Change data type

--- javascripts/order.js
+++ javascripts/order.js
@@ -46,11 +46,12 @@ $(document).ready(function(){
 			//form.submit();
 			//var d = {'formkey' : '', $(form).serialize()};
 			var v = $('#order-form').jsonify({stringify:true});
-			var d = {"entry.1877278473":"Fresh mushroom","entry.1204439759":"1","entry.1611368027":"Henry Addo","entry.1332200273":"henry@addhen.org","entry.1125117768":"09083453","entry.1132168120":"ACC 106","entry.879379036":"Arusha","entry.324110172":"1"}
-			console.log(v);
+			var d = $(form).serialize();
+			console.log(d);
 			var request = $.ajax({
-				type: "POST",
-				dataType: "jsonp",
+				type: 'POST',
+				dataType: 'text/html',
+				crossDomain: true,
 				url: "https://docs.google.com/forms/d/1i47I61djvq8eHTxNI-0cMOSpjO-R1yvjv9k5MMuupJ0/formResponse",
 				data: d,
 				statusCode: {
