f7ca1fa737764da7f7db6c7340caa795fff89920
Henry Addo <addhen@linuxmail.org>
2014-05-06 06:46:27 +0900

    Made copyright current year dynamic

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function ($) {
-jQuery('.totop').click(function(){
-	jQuery('html, body').animate({ scrollTop: 0 }, "slow");
-});
+	jQuery('.totop').click(function(){
+		jQuery('html, body').animate({ scrollTop: 0 }, "slow");
+	});
+
+	// full year
+	jQuery(".year").text( (new Date).getFullYear() );
 });
